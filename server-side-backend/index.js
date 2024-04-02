924ff61c72c2d23c3843dffcb302b0c444d370e2
AlvaroVelascoPrieto <alvaritovelasco2002@gmail.com>
2024-04-02 08:14:36 +0000

    Backend for put requests implemented

--- server-side-backend/index.js
+++ server-side-backend/index.js
@@ -35,8 +35,9 @@ app.route('/')
             console.log(err);
             res.json({
                     'code': 500,
-                    'message': "There was an server error."
+                    'message': "There was a server error."
             });
+	    console.log('There was a server error.')
         } else {
 
             if (results.length > 0) {
@@ -45,14 +46,44 @@ app.route('/')
                     'message': 'Get values with success.',
                     'data': results[0]
                 });
+		console.log('Log in successful')
             } else {
                 res.json({
                     'code': 300,
-                    'message': 'There are no users in the database with that values.',
+                    'message': 'There are no users in the database with those values.',
                 });
+		console.log('There are no users in de database with those values')
             }
         }
     })
+})
+
+.post((req, res) => {
+  let dni = req.body.dni;
+  let contrasena = req.body.contrasena;
+  let nombre = req.body.nombre;
+  let apellido = req.body.apellido;
+  let telefono = req.body.telefono;
+  console.log(dni)
+
+  let query_registrarse = "INSERT INTO `USUARIO` VALUES(?, ?, ?, ?, ?)"
+
+  connection.query(query_registrarse, [dni, contrasena, nombre, apellido, telefono], 
+    (err, results, fields) => {
+        if (err) {
+            res.json({
+                'code': 300,
+                'message': 'There was an error while trying to sign up'
+            });
+	    console.log('There was an error while trying so sign up')
+        } else {
+            res.json({
+                'code': 200,
+                'message': 'Successful sign up'
+            });
+	    console.log('Succesful sign up')
+        }
+    });
 })
 
 // Starting to listen
